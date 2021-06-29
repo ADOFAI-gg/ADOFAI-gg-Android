399fcf6e315777ec38b70e2f8301d083a65fb2b2
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-06-29 13:44:07 +0900

    Feat: Create navigation graph

--- app/src/main/java/io/luxus/adofai/util/Constants.kt
+++ app/src/main/java/io/luxus/adofai/util/Constants.kt
@@ -1,4 +1,16 @@
 package io.luxus.adofai.util
 
 object Constants {
+
+    // usage : BASE_URL/tq?key=KEY&gid=GID
+
+    const val BASE_URL = "https://spreadsheets.google.com"
+
+    const val KEY_USER = "1PzLHfWmVWJHrBGnNSsLTsdH0ibdk0hB4MpKHET1nkpU"
+    const val KEY_ADMIN = "1MOz5cmMpYwpBB95DK1Udcti_8eOrswnxWzFurhAz0yg"
+
+    const val GID_ADMIN_MAPS = "739034057"
+    const val GID_ADMIN_RANKING = "151952522"
+
+
 }
