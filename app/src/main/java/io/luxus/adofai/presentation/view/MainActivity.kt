6983f2c3ff568a6fcd6745b907b386015c5ed4e3
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-06-29 11:36:50 +0900

    Init: Init MVVM + R + Clean Architecture structure

--- app/src/main/java/io/luxus/adofai/presentation/view/MainActivity.kt
+++ app/src/main/java/io/luxus/adofai/presentation/view/MainActivity.kt
@@ -1,11 +1,16 @@
-package io.luxus.adofai
+package io.luxus.adofai.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import io.luxus.adofai.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
