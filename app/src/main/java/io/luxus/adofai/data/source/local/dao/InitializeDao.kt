e8070a3bc774f1acb01172c9b3f6a7190ff16f26
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-06-30 20:42:39 +0900

    Feat: Create InitializeDao

--- app/src/main/java/io/luxus/adofai/data/source/local/dao/InitializeDao.kt
+++ app/src/main/java/io/luxus/adofai/data/source/local/dao/InitializeDao.kt
@@ -1,21 +1,62 @@
 package io.luxus.adofai.data.source.local.dao
 
-import androidx.room.Dao
-import androidx.room.Query
-import androidx.room.Transaction
+import androidx.room.*
+import io.luxus.adofai.data.source.local.entity.*
+import io.luxus.adofai.data.source.local.entity.relation.LevelCreatorCrossRef
+import io.luxus.adofai.data.source.local.entity.relation.LevelTagCrossRef
+import io.luxus.adofai.data.source.local.entity.relation.SongArtistCrossRef
 
 @Dao
 abstract class InitializeDao {
 
     @Transaction
-    open fun initDatabase() {
+    open fun initDatabase(personList: List<Person>, songList: List<Song>, tagList: List<Tag>,
+                          levelList: List<Level>, playLogList: List<PlayLog>,
+                          songArtistCrossRefList: List<SongArtistCrossRef>,
+                          levelTagCrossRefList: List<LevelTagCrossRef>,
+                          levelCreatorCrossRefList: List<LevelCreatorCrossRef>) {
         clearDatabase()
-
+        insertPerson(personList)
+        insertSong(songList)
+        insertTag(tagList)
+        insertLevel(levelList)
+        insertPlayLog(playLogList)
+        insertSongArtistCrossRef(songArtistCrossRefList)
+        insertLevelTagCrossRef(levelTagCrossRefList)
+        insertLevelCreatorCrossRef(levelCreatorCrossRefList)
     }
 
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertPerson(personList: List<Person>)
 
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertSong(songList: List<Song>)
 
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertTag(tagList: List<Tag>)
 
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertLevel(levelList: List<Level>)
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertPlayLog(playLogList: List<PlayLog>)
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertSongArtistCrossRef(songArtistCrossRefList: List<SongArtistCrossRef>)
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertLevelTagCrossRef(levelTagCrossRefList: List<LevelTagCrossRef>)
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertLevelCreatorCrossRef(levelCreatorCrossRefList: List<LevelCreatorCrossRef>)
 
     @Transaction
     open fun clearDatabase() {
