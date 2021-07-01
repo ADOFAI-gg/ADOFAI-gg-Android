7b237d8210254629e1e8719b187c8126f57ee428
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-01 15:58:40 +0900

    Test: Init test code structure

---
+++ app/src/androidTest/java/io/luxus/adofai/CustomTestRunner.kt
@@ -0,0 +1,15 @@
+package io.luxus.adofai
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+// https://developer.android.com/training/dependency-injection/hilt-testing
+class CustomTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.canonicalName, context)
+    }
+
+}
---
+++ app/src/androidTest/java/io/luxus/adofai/HiltTestApplication.kt
@@ -0,0 +1,6 @@
+package io.luxus.adofai
+
+import dagger.hilt.android.testing.CustomTestApplication
+
+//@CustomTestApplication(App::class)
+//interface HiltTestApplication
