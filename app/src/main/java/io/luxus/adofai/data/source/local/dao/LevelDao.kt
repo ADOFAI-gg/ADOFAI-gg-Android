fc1ce047e8d760536fe1673ffd1dbc7ecb3a2e18
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-01 12:55:18 +0900

    Feat: Add transaction to each pojo query

--- app/src/main/java/io/luxus/adofai/data/source/local/dao/LevelDao.kt
+++ app/src/main/java/io/luxus/adofai/data/source/local/dao/LevelDao.kt
@@ -2,6 +2,7 @@ package io.luxus.adofai.data.source.local.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import io.luxus.adofai.data.source.local.entity.pojo.LevelWithSongNCreator
 import io.luxus.adofai.data.source.local.entity.pojo.SongWithArtist
 
@@ -10,29 +11,35 @@ abstract class LevelDao {
 
     // need function
     // order by id (asc / desc)
-    // order by level (asc / desc)
+    // order by localLevel (asc / desc)
     // order by creator name (asc / desc)
-    // order by song name (asc / desc)
+    // order by localSong name (asc / desc)
     // order by artist (asc / desc)
     // order by max / min bpm (asc / desc)
     // order by tile amount (asc / desc)
 
-    @Query("SELECT * FROM level WHERE levelId = :id;")
+    @Transaction
+    @Query("SELECT * FROM localLevel WHERE levelId = :id;")
     abstract fun getLevelWithSongNCreator(id: Long): LevelWithSongNCreator
 
-    @Query("SELECT * FROM level ORDER BY levelId ASC;")
+    @Transaction
+    @Query("SELECT * FROM localLevel ORDER BY levelId ASC;")
     abstract fun getOrderByIdAsc(): LevelWithSongNCreator
 
-    @Query("SELECT * FROM level ORDER BY levelId DESC;")
+    @Transaction
+    @Query("SELECT * FROM localLevel ORDER BY levelId DESC;")
     abstract fun getOrderByIdDesc(): LevelWithSongNCreator
 
-    @Query("SELECT * FROM level ORDER BY level ASC;")
+    @Transaction
+    @Query("SELECT * FROM localLevel ORDER BY level ASC;")
     abstract fun getOrderByLevelAsc(): LevelWithSongNCreator
 
-    @Query("SELECT * FROM level ORDER BY level DESC;")
+    @Transaction
+    @Query("SELECT * FROM localLevel ORDER BY level DESC;")
     abstract fun getOrderByLevelDesc(): LevelWithSongNCreator
 
-    @Query("SELECT * FROM song WHERE songId = :id;")
+    @Transaction
+    @Query("SELECT * FROM localSong WHERE songId = :id;")
     abstract fun getSongWithArtist(id: Long): SongWithArtist
 
 }
